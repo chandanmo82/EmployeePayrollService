93961dab907c07472d6b531cbc8efcc56e373be2
Chandan <chandanmohanty11111>
2021-10-29 19:46:19 +0530

    REFACTORED:Method to write to file

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
@@ -1,29 +1,22 @@
 package com.bridgelabz.employeepayrollservice;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class EmployeePayrollService {
-    public enum IoService {
+
+    public enum IOService {
         CONSOLE_IO, FILE_IO, DB_IO, REST_IO
     }
 
     private List<EmployeePayrollData> employeePayrollList;
 
+    public EmployeePayrollService() {
+    }
+
     public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
         this.employeePayrollList = employeePayrollList;
-    }
-    public static void main(String[] args) {
-
-        System.out.println("----------- Welcome To Employee Payroll Application ----------\n");
-        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-        Scanner consoleInputReader = new Scanner(System.in);
-
-        employeePayrollService.readEmployeePayrollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayrollData();
-
     }
 
     private void readEmployeePayrollData(Scanner consoleInputReader) {
@@ -38,9 +31,24 @@ public class EmployeePayrollService {
         employeePayrollList.add(new EmployeePayrollData(id, name, salary));
     }
 
-    private void writeEmployeePayrollData() {
-        System.out.println("\nWriting Employee Payroll Roster to Console\n" + employeePayrollList);
+    public void writeEmployeePayrollData(IOService ioService) {
+        if(ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("\nWriting Employee Payroll Roster to Console\n" + employeePayrollList);
+
+        else if(ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("---------- Welcome To Employee Payroll Application ----------\n");
+        ArrayList<EmployeePayrollData> employeePayrollList  = new ArrayList<EmployeePayrollData>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
+
     }
 
 }
-
