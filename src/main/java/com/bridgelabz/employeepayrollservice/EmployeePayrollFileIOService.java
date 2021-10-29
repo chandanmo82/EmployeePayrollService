306d3314208f951b76cd865bd37b48fa3396cab2
Chandan <chandanmohanty11111>
2021-10-29 19:59:37 +0530

    REFACTORED:Method to print and count entries in file

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollFileIOService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollFileIOService.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.employeepayrollservice;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class EmployeePayrollFileIOService {
+
     public static String PAYROLL_FILE_NAME = "payroll-file.txt";
 
     public void writeData(List<EmployeePayrollData> employeePayrollList) {
@@ -19,10 +21,29 @@ public class EmployeePayrollFileIOService {
         try {
             Files.write(Paths.get(PAYROLL_FILE_NAME), empBuffer.toString().getBytes());
 
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-}
 
+    public void printData() {
+
+        try {
+            Files.lines(new File(PAYROLL_FILE_NAME).toPath()).forEach(System.out::println);
+        }
+        catch(IOException e) {e.printStackTrace();}
+
+    }
+
+    public long countEntries() {
+
+        long entries=0;
+        try {
+            entries = Files.lines(new File(PAYROLL_FILE_NAME).toPath()).count();
+        }
+        catch(IOException e) {e.printStackTrace();};
+        return entries;
+    }
+}
