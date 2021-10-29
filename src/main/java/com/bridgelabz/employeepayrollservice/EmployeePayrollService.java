b61447f122b497ddb0e09f29bd50a6593e77d436
Chandan <chandanmohanty11111>
2021-10-29 18:34:46 +0530

    Initial Commit

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.employeepayrollservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    public enum IoService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+    }
+
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("---------- Welcome To Employee Payroll Application ----------\n");
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+
+        System.out.println("Enter the Employee Id : ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter the Employee Name : ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter the Employee Salary : ");
+        double salary = consoleInputReader.nextDouble();
+
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roster to Console\n" + employeePayrollList);
+    }
+
+}
+
