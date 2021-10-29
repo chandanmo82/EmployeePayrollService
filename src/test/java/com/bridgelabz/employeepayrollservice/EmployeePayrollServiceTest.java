7bb31643fc7801d340c976b5e9d252654cc4850b
Chandan <chandanmohanty11111>
2021-10-29 20:22:43 +0530

    REFACTORED:Testing ability to read from file

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -26,4 +26,10 @@ public class EmployeePayrollServiceTest
         Assert.assertEquals(3, entries);
 
     }
+    @Test
+    public void givenFile_WhenRead_ShouldReturnNumberOfEntries() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        long entries = employeePayrollService.readDataFromFile(IOService.FILE_IO);
+        Assert.assertEquals(3, entries);
+    }
 }
